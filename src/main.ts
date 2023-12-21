2f0834459b47deecf118abdfc3a45211ae670660
Tyler Mayoff <tyler@tylermayoff.com>
2023-12-21 14:22:36 -0500

    trying a new vite wasm plugn

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import './app.css'
 import App from './App.svelte'
 
-import init from '../bevy_sprites/pkg';
+import init from "../bevy_sprites/Cargo.toml";
+
+if (!import.meta.env.SSR) {
+  init().then((module: any) => {
+    console.log("Loaded WASM");
+    module.run();
+  });
+}
 
 const app = new App({
   target: document.getElementById('app'),
 })
-
-init().then((res) => {
-  console.log('init wasm-pack');
-  res.run();
-});
 
 export default app
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import wasmPack from "vite-plugin-wasm-pack";
+import rust from '@wasm-tool/rollup-plugin-rust';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
-    wasmPack("bevy_sprites")
+    rust({
+      verbose: true,
+      serverPath: "build/"
+    })
   ],
 })
